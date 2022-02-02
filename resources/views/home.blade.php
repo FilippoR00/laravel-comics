58c440e8e50842fd19b35fed982066fa22a24bb6
Filippo Rossi <rossifilippo2000@gmail.com>
2022-02-02 17:32:47 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.base')
+
+@section('pageTitle')
+    Home
+@endsection
+
+@section('pageContent')
+    <div class="main">
+        <div class="jumbo"></div>
+        <div class="container current-series">
+            <h2 class="label">Current series</h2>
+            <div class="box">
+                @foreach ($comics as $comic)
+                    <div class="card">
+                        <div class="img">
+                            <img src="{{$comic["thumb"]}}" alt="{{$comic["type"]}}">
+                        </div>
+                        <div class="title">{{$comic["series"]}}</div>
+                    </div>
+                @endforeach
+                <button><a href="#">Load more</a></button>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="topbar"></div>
+    <div class="container header">
+        <div class="logo">
+            <img src="{{asset("img/dc-logo.png")}}" alt="">
+        </div>
+        <div class="nav-menu">
+            <ul>
+                <li>
+                    <a href="#">characters</a>
+                </li>
+                <li class="true">
+                    <a href="#">comics</a>
+                </li>
+                <li>
+                    <a href="#">movies</a>
+                </li>
+                <li>
+                    <a href="#">tv</a>
+                </li>
+                <li>
+                    <a href="#">games</a>
+                </li>
+                <li>
+                    <a href="#">collectibles</a>
+                </li>
+                <li>
+                    <a href="#">videos</a>
+                </li>
+                <li>
+                    <a href="#">fans</a>
+                </li>
+                <li>
+                    <a href="#">news</a>
+                </li>
+                <li>
+                    <a href="#">shop</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
